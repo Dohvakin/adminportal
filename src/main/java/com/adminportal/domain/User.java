ac06543471a3f152f08bf6100b2a54e689c32e08
Dohvakin <yashkaveeshwar@gmail.com>
2019-03-01 21:50:59 +0530

    Fixing Hibernate auto Incrementation
    Signed-off-by: Dohvakin <yashkaveeshwar@gmail.com>

--- src/main/java/com/adminportal/domain/User.java
+++ src/main/java/com/adminportal/domain/User.java
@@ -15,7 +15,7 @@ import java.util.Set;
 public class User implements UserDetails {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", nullable = false, updatable = false)
 	private Long id;
 	private String username;
